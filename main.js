61cd3ea8d0978e1f7860871f484791f8b3377403
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2024-06-14 21:35:53 +0700

    initialize electron, react, and tailwind workspace

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('path')
+const url = require('url')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    },
+    title: "PHiscord",
+  })
+
+  // const startUrl = url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: 'file',
+  // })
+
+  win.loadURL('http://localhost:5173')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
